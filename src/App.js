80c82126028c884955d20d7c03a87daf6afa7f19
tyranoboy1 <gkwlgnd@naver.com>
2023-09-14 14:27:28 +0900

    react study ch6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import Say from "./Components/Say";
+// //function
+// import EventPractice from "./Components/EventPractice";
+// import EventPracticeClass from "./Components/EventPracticeClass";
+// import Say from "./Components/Say";
+// import ValidationSample from "./Components/ValidationSample";
 
-function App() {
-  return <Say />;
+// function App() {
+//   return <ValidationSample />;
+// }
+
+// export default App;
+
+//class
+// import React, { Component } from "react";
+// import ScrollBox from "./Components/ScrollBox";
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+//         <button onClick={() => this.scrollBox.scrollToBottom()}>
+//           맨 밑으로
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+
+import React, { Component } from "react";
+import ScrollBox from "./Components/ScrollBox";
+import IterationSample from "./Components/IterationSample";
+class App extends Component {
+  render() {
+    return <IterationSample />;
+  }
 }
-
 export default App;
