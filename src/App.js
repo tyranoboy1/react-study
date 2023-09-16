31851886692d9dee628349d14b4d8d9d6894dd2c
tyranoboy1 <gkwlgnd@naver.com>
2023-09-16 10:50:49 +0900

    react router study

--- src/App.js
+++ src/App.js
@@ -94,9 +94,33 @@ import { useState } from "react";
 import CounterR from "./Components/CounterR";
 import InfoR from "./Components/InfoR";
 import Average from "./Components/Average";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
+import Layout from "./Layout";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
-  return <Average />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
